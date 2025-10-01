2f7cb72a7bda468c8055306c08050bb882da7c85
Aronatto <koksik122@o2.pl>
2025-10-01 11:49:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,15 +2,20 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class  # 1 to 7
         self.clean_mark = clean_mark        # 1 to 10
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
